568a5dd1c26d6dfb8160f224399c5a8a9e21a5a6
Anatoly Duzenko <anatoliy.duzenko@gmail.com>
2025-04-06 10:37:35 -0400

    Added / updated Jobs tests

--- tests/Feature/Jobs/CheckEndpointJobTest.php
+++ tests/Feature/Jobs/CheckEndpointJobTest.php
@@ -7,6 +7,7 @@ use App\Models\Endpoint;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Jobs\CheckEndpointJob;
+use Illuminate\Support\Facades\Cache;
 use Tests\TestCase;
 
 class CheckEndpointJobTest extends TestCase
@@ -15,6 +16,8 @@ class CheckEndpointJobTest extends TestCase
 
     public function test_it_logs_successful_response()
     {
+        Cache::flush();
+
         Http::fake([
             'https://example.com' => Http::response([], 200)
         ]);
@@ -36,6 +39,8 @@ class CheckEndpointJobTest extends TestCase
 
     public function test_it_logs_failure_when_request_fails()
     {
+        Cache::flush();
+        
         Http::fake([
             'https://broken.com' => Http::sequence()->pushStatus(500),
         ]);
