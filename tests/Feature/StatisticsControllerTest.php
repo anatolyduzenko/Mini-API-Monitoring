d956b363393d99aec97d56604099218b5b6fc6cc
Anatoly Duzenko <anatoliy.duzenko@gmail.com>
2025-04-27 04:32:19 -0400

    Fix for Statistics controller. Requests require authorization now

--- tests/Feature/StatisticsControllerTest.php
+++ tests/Feature/StatisticsControllerTest.php
@@ -4,12 +4,16 @@ use App\Enums\SplitType;
 use App\Enums\StatusCode;
 use App\Services\LogsService;
 use App\Services\StatisticsService;
+use App\Models\User;
 use Tests\TestCase;
 
 class StatisticsControllerTest extends TestCase
 {
     public function test_get_uptime_returns_data()
     {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
         $mockData = ['uptime' => 99.9];
         $mockService = $this->createMock(StatisticsService::class);
         $mockService->method('reportUptime')->willReturn($mockData);
@@ -30,6 +34,9 @@ class StatisticsControllerTest extends TestCase
 
     public function test_get_recent_logs_returns_data()
     {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
         $mockData = [['message' => 'OK']];
         $mockLogs = $this->createMock(LogsService::class);
         $mockLogs->method('recentLogs')->willReturn($mockData);
@@ -45,6 +52,9 @@ class StatisticsControllerTest extends TestCase
 
     public function test_get_uptime_graph_returns_transformed_data()
     {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
         $mockTrendData = collect([
             (object) [
                 'date' => '2024-04-01',
@@ -82,6 +92,9 @@ class StatisticsControllerTest extends TestCase
 
     public function test_get_response_time_returns_transformed_data()
     {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        
         $mockResponseTime = collect([
             (object) [
                 'date' => '2024-04-01',
