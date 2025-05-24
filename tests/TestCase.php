b874c629400a16e46a5e65d6598b389c2b57057e
Anatoly Duzenko <8387069+anatolyduzenko@users.noreply.github.com>
2025-05-24 19:46:04 +0400

    Broadcast notifications (#20)
    * Added broadcasting. Refactored services, added extensible monitoring for broadcasting
    
    * Fix for new monitoring provider. Remove incorrect binding in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,19 +10,5 @@ class TestCase extends BaseTestCase
     protected function setUp(): void
     {
         parent::setUp();
-
-        $this->app->bind(MetricManager::class, fn () => new class
-        {
-            public function set(string $namespace, string $name, $value, array $labels = []): void {}
-
-            public function register(array $metricGroups): void {}
-
-            public function getMetric(string $name): mixed
-            {
-                return null;
-            }
-
-            public function collect(): void {}
-        });
     }
 }
